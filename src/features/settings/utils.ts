c2465602a1d39b2d44376e9fae6feea01ec83968
zalentec-proj <zalentec@gmail.com>
2026-08-19 11:54:21 -0300

    Atualiza WhatsApp público

--- src/features/settings/utils.ts
+++ src/features/settings/utils.ts
@@ -1,5 +1,5 @@
-export const LEEHOV_WHATSAPP_NUMBER = "5545999860067";
-export const LEEHOV_WHATSAPP_DISPLAY = "(45) 99986-0067";
+export const LEEHOV_WHATSAPP_NUMBER = "5545999078380";
+export const LEEHOV_WHATSAPP_DISPLAY = "+55 45 99907-8380";
 export const LEEHOV_WHATSAPP_URL = `https://wa.me/${LEEHOV_WHATSAPP_NUMBER}`;
 
 export function buildWhatsAppUrl(phone: string, message: string) {
